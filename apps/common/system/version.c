51712dd206536f165499f2313454b486b19a6175
fengshunjian <fengshunjian@zh-jieli.com>
2023-02-21 21:57:26 +0800

    | **---------------------------------AC79NN_SDK_V1.0.2.8---------------------------------** * 修复错误过滤arp response包导致外网不通问题; * 修复有key时头部解密出错导致OTA升级失败; * 修复长文件名匹配存在概率匹配出错 && 增加读写buf同步; * 修复vm整理过程中突然断电造成数据丢失问题;

--- apps/common/system/version.c
+++ apps/common/system/version.c
@@ -7,7 +7,7 @@ extern char __VERSION_END[];
 
 const char *sdk_version(void)
 {
-    return "AC79NN SDK on branch [release/AC79NN_SDK_V1.0.2] tag [AC79NN_SDK_V1.0.2.7] ";
+    return "AC79NN SDK on branch [release/AC79NN_SDK_V1.0.2] tag [AC79NN_SDK_V1.0.2.8] ";
 }
 
 static int app_version_check()
